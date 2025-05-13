63b4c0d49e4cc6cb0c6d693bb801bf4dd3b6a6f9
Mickael <mickael.mesloub@gmail.com>
2025-05-13 17:10:53 +0200

    ex 2 Sorcellerie

--- ecole_sorcellerie/src/Main.java
+++ ecole_sorcellerie/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Sorcier harry = new Sorcier("Potter", 11);
+        Sorcier dumby = new Sorcier("Dumbledore", 154);
+        Sorcier severus = new Sorcier("Rogue", 33);
+
+        System.out.println(harry);
+        System.out.println(dumby);
+        System.out.println(severus);
+
     }
 }
---
+++ ecole_sorcellerie/src/Sorcier.java
@@ -0,0 +1,31 @@
+public class Sorcier {
+    private String nom;
+    private int age;
+
+    public Sorcier(final String nom, final int age) {
+        this.nom = nom;
+        this.age = age;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (%d ans)", nom, age);
+    }
+
+}
